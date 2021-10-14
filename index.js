ee230fba88600dc80c95e5d62071dd2a4bcbac46
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-10-13 23:45:13 -0500

    Ahora cuando se envía un formulario, dirige a la persona a una página de confirmacion, y le ofrece un boton de volver al inicio

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 $(document).ready(function() {
 
-    // Primordiales
-  $("#homepageLink").attr("href", "/");
-  $("#logoNavbar").attr("src", "/i_temas/Logo Lernist.png")
-  $(".Inicio").text("Inicio");
-  $("#aprenderLink").text("Aprender");
-  $("#aprenderLink").attr("href", "/aprender.html")
-  $("#enseñarLink").hide();
-  $("#enseñarLink").text("Enseñar");
-  $("#enseñarLink").attr("href", "/enseñar.html")
-
-  $("#fraseFooter").text("Aprende cosas Increíbles, con contenido Increíble.");
-  $("#footerFacebook").attr("href", "https://www.facebook.com/LernistOficial");
-  $("#footerTwitter").attr("href", "https://twitter.com/Lernist_Oficial");
-  $("#footerInstagram").attr("href", "https://www.instagram.com/Lernist_Oficial/");
-  $("#footerContacto").attr("href", "/contacto.html");
-  $("#footerContacto").text("Contacto");
-  $("#footerNosotros").attr("href", "/nosotros.html");
-  $("#footerNosotros").text("Nosotros");
-  $("#footerAnunciantes").attr("href", "");
-  $("#footerAnunciantes").text("Anunciantes");
-  $("#footerAnunciantes").hide();
-
   $("#menuHamburguesa").click(function() {
     $(".links-principales").slideToggle(400);
     $(".bar1").toggleClass("change-bar1");
