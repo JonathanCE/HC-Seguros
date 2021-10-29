5326f82a0375839f3c6fc593f3395165bcfa717e
JonathanCE <jonatha.CE.2025@protonmail.com>
2021-10-29 01:06:40 -0500

    Se terminó funcionalmente el sitio de mi papá, se añadieron las descripciones de cada servicio

--- index.js
+++ index.js
@@ -6,5 +6,21 @@ $(document).ready(function() {
     $(".bar2").toggleClass("change-bar2");
     $(".bar3").toggleClass("change-bar3");
   });
+
+  $(".boton-seguros").click(function() {
+    $(".extra-seguros").slideToggle();
+    $(".ver-mas-seguros").toggle();
+    $(".ver-menos-seguros").toggle();
+  });
+  $(".boton-ahorros").click(function() {
+    $(".extra-ahorro").slideToggle();
+    $(".ver-mas-ahorros").toggle();
+    $(".ver-menos-ahorros").toggle();
+  });
+  $(".boton-inversiones").click(function() {
+    $(".extra-inversiones").slideToggle();
+    $(".ver-mas-inversiones").toggle();
+    $(".ver-menos-inversiones").toggle();
+  });
   
 });
